254ff7acf6728755d45d586808712343f456428d
Albert <ag@thehotelsnetwork.com>
2021-05-14 17:10:59 +0200

    removed unused code from example

--- main.c
+++ main.c
@@ -63,28 +63,9 @@ static uchar    idleRate;   /* repeat rate for keyboards, never used for mice */
 
 static uint16_t nes_controller_state;
 
-usbMsgLen_t usbFunctionSetup(uchar data[8])
-{
-usbRequest_t    *rq = (void *)data;
-
-	/* The following requests are never used. But since they are required by
-	 * the specification, we implement them in this example.
-	 */
-	if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
-		if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
-			/* we only have one report type, so don't look at wValue */
-			usbMsgPtr = (void *)&reportBuffer;
-			return sizeof(reportBuffer);
-		}else if(rq->bRequest == USBRQ_HID_GET_IDLE){
-			usbMsgPtr = &idleRate;
-			return 1;
-		}else if(rq->bRequest == USBRQ_HID_SET_IDLE){
-			idleRate = rq->wValue.bytes[1];
-		}
-	}else{
-		/* no vendor specific requests implemented */
-	}
-	return 0;   /* default for not implemented requests: return no data back to host */
+// implementation required, but not used
+usbMsgLen_t usbFunctionSetup(uchar data[8]) {
+	return 0;
 }
 
 void process_buttons() {
@@ -136,13 +117,7 @@ int __attribute__((noreturn)) main(void) {
 	snes_init();
 
 	wdt_enable(WDTO_1S);
-	/* Even if you don't use the watchdog, turn it off here. On newer devices,
-	 * the status of the watchdog (on/off, period) is PRESERVED OVER RESET!
-	 */
-	/* RESET status: all port bits are inputs without pull-up.
-	 * That's the way we need D+ and D-. Therefore we don't need any
-	 * additional hardware initialization.
-	 */
+	
 	usbInit();
 	usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
 	i = 0;
